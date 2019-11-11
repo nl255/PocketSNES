63053b5b221678fb02e18a12e20c464eb4777c9f
Soar Qin <soarchin@gmail.com>
2019-11-11 13:18:22 +0800

    revert some changes

--- pocketsnes/snes9x/gfx.cpp
+++ pocketsnes/snes9x/gfx.cpp
@@ -3638,7 +3638,19 @@ void S9xUpdateScreen ()
     GFX.r212d = Memory.FillRAM [0x212d];
     GFX.r2130 = Memory.FillRAM [0x2130];
 
-    GFX.Pseudo = (Memory.FillRAM [0x2133] & 8) != 0;
+#ifdef JP_FIX
+
+    GFX.Pseudo = (Memory.FillRAM [0x2133] & 8) != 0 &&
+				 (GFX.r212c & 15) != (GFX.r212d & 15) &&
+				 (GFX.r2131 == 0x3f);
+
+#else
+
+    GFX.Pseudo = (Memory.FillRAM [0x2133] & 8) != 0 &&
+		(GFX.r212c & 15) != (GFX.r212d & 15) &&
+		(GFX.r2131 & 0x3f) == 0;
+
+#endif
 	
     if (IPPU.OBJChanged)
 		S9xSetupOBJ ();
